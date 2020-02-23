6a32ccb63f9e941a07c83d74a012eef1e167f4a4
Vladimir-Petrovsky <vova.petrovckij@gmail.com>
2020-02-23 08:30:13 +0200

    home_work_05

---
+++ hw-05/js/task-03.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class Storage {
+  constructor(products) {
+    this.items = products;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    let idx = this.items.indexOf(item);
+    this.items.splice(idx, 1);
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
