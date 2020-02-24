bf1c6e3bd1117a35220513b33151d31bca1b93b1
Vladimir-Petrovsky <vova.petrovckij@gmail.com>
2020-02-24 20:49:29 +0200

    home_work_06

---
+++ hw-06/js/task-08.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import { default as users } from './users.js';
+
+const getUsersWithFriend = (users, friendName) => {
+  return users
+    .filter(user => user.friends.includes(friendName))
+    .map(friend => friend.name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
---
+++ hw-06/js/task-09.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import { default as users } from './users.js';
+
+const getNamesSortedByFriendsCount = users => {
+  return users
+    .sort((userA, userB) => userA.friends.length - userB.friends.length)
+    .map(user => user.name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
